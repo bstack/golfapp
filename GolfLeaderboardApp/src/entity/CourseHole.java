cf636e1d765da8fa64bc9ac5a6bdea1bd8a61390
bstack <bstack@continuumcommerce.com>
2013-08-12 23:44:52 +0100

    Added course hole entity - and concept

---
+++ GolfLeaderboardApp/src/entity/CourseHole.java
@@ -0,0 +1,72 @@
+package entity;
+
+public class CourseHole {
+	private int id;
+	private int CourseId;
+	private int whiteYards;
+	private int whitePar;
+	private int yellowYards;
+	private int yellowScoreIndex;
+	private int redYards;
+	private int redPar;
+	private int redScoreIndex;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getCourseId() {
+		return CourseId;
+	}
+	public void setCourseId(int courseId) {
+		CourseId = courseId;
+	}
+	public int getWhiteYards() {
+		return whiteYards;
+	}
+	public void setWhiteYards(int whiteYards) {
+		this.whiteYards = whiteYards;
+	}
+	public int getWhitePar() {
+		return whitePar;
+	}
+	public void setWhitePar(int whitePar) {
+		this.whitePar = whitePar;
+	}
+	public int getYellowYards() {
+		return yellowYards;
+	}
+	public void setYellowYards(int yellowYards) {
+		this.yellowYards = yellowYards;
+	}
+	public int getYellowScoreIndex() {
+		return yellowScoreIndex;
+	}
+	public void setYellowScoreIndex(int yellowScoreIndex) {
+		this.yellowScoreIndex = yellowScoreIndex;
+	}
+	public int getRedYards() {
+		return redYards;
+	}
+	public void setRedYards(int redYards) {
+		this.redYards = redYards;
+	}
+	public int getRedPar() {
+		return redPar;
+	}
+	public void setRedPar(int redPar) {
+		this.redPar = redPar;
+	}
+	public int getRedScoreIndex() {
+		return redScoreIndex;
+	}
+	public void setRedScoreIndex(int redScoreIndex) {
+		this.redScoreIndex = redScoreIndex;
+	}
+	
+	
+
+}
